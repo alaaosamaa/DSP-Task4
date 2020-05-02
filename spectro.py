2660aa7c2463d822b7ced01677d3009903e34e8d
alaaosamaa <youremail@domain.com>
2020-05-02 22:29:23 +0200

    spectro

--- spectro.py
+++ spectro.py
@@ -1,24 +1,36 @@
-from PyQt5 import QtWidgets 
-from PyQt5 import QtCore 
-from PyQt5 import QtGui
-from gui import Ui_MainWindow
-import sys
 
-class MainWindow(QtWidgets.QMainWindow):
+import matplotlib.pyplot as plt
+import pylab
+from scipy import signal
+from scipy.io import wavfile
+# import librosa
+# import librosa.display
+# import IPython.display as ipd
 
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+sample_rate, samples = wavfile.read('Songs\eminem-lose_yourself001.wav')
+samples=samples[0:60*sample_rate]
+spectro, freqs, time, img = plt.specgram(samples, NFFT=None, Fs=sample_rate)
+print(spectro)
+plt.show()
+# x , sr = librosa.load("Songs\eminem-lose_yourself001.wav")
+
+# librosa.display.waveplot(x, sr=sr)
+#display Spectrogram
+# X = librosa.stft(x)
+# Xdb = librosa.amplitude_to_db(abs(X))
+# plt.figure(figsize=(14, 5))
+# librosa.display.specshow(Xdb, sr=10000, x_axis='time', y_axis='hz') 
+#If to pring log of frequencies  
+#librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='log')
+# plt.colorbar()
+# plt.show()
+# print("load")
+# print(type(x), type(sr))
 
 
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    main = MainWindow()
-    main.show()
- 
-    sys.exit(app.exec_())
 
-
-if __name__ == '__main__':
-    main()
+# scipy
+# frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
+# plt.pcolormesh(times, frequencies, spectrogram)
+# plt.ylabel('Frequency [Hz]')
+# plt.xlabel('Time [sec]')
